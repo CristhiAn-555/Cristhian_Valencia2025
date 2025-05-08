a66606e111aa18186b4cdc4927d978332b57288e
CristhiAn-555 <cristhianvalencia999@gmail.com>
2025-05-08 17:15:32 -0500

    practica

---
+++ src/main/java/ec/edu/espoch/cristhianvalencia2025/CristhianValencia2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.cristhianvalencia2025;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class CristhianValencia2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
